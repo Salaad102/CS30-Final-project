67465e494fb3c1cc0cbd0828c008605c486b968a
Salaad102 <salaara102@gmail.com>
2022-11-22 12:13:33 -0600

    Renamed game, Making basic class

--- Plants Vs Zombies/sketch.js
+++ Plants Vs Zombies/sketch.js
@@ -1,6 +1,6 @@
-// Project Title
-// Your Name
-// Date
+// Plants Vs Zombies
+// Salaar Ahmed
+// November 22, 2022
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
