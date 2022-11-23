59d287b92592a00676a731acbc303b42583b7a46
Salaad102 <salaara102@gmail.com>
2022-11-23 12:14:20 -0600

    Added p5.play library and created a 2d array

--- Plants Vs Zombies/sketch.js
+++ Plants Vs Zombies/sketch.js
@@ -14,12 +14,20 @@ function draw() {
   background(220);
 }
 
-function create2dArray(){
-  for 
+function create2dArray(COLS, ROWS) {
+  let emptyArray = [];
+  for (let y=0; y<ROWS; y++){
+    emptyArray.push([]);
+    for (let x=0; x<COLS; x++){
+      emptyArray[y].push(0);
+    
+    }
+    return emptyArray;
+  }
 }
 
 class Plants {
-  constructor(x, y,){
+  constructor(x, y){
     this.x = x;
     this.y = y;
     
